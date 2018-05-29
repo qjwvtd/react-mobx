2f32fa42628cfbd118728fbd249ca58e7f45bad7
qjwvtd <626288qqQQQQ>
2018-05-29 22:30:07 +0800

    修改

--- web/js/app.js
+++ web/js/app.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {observer} from 'mobx-react';
-import {User,PackageJson} from './store';
+import {User,Company} from './store';
 
 const user = new User();
-const packageJson = new PackageJson();
-
-class AddUser extends Component{
+const company = new Company();
+class PlayUser extends Component{
     constructor(props){
         super(props);
         this.state = {};
@@ -34,33 +33,80 @@ class AddUser extends Component{
         );
     }
 }
-class ChangePackage extends Component{
-    changePackage(){
-        const ran = ((Math.random()*10).toFixed(0)) + '.0.0';
-        packageJson.changeVersion(ran);
-        packageJson.changeDescription('the demo version is ' + ran);
+class QueryCompany extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    findCompany(){
+        //const cName = this.refs.companyName.value.replace(/\s+/g,"");
+        //if(cName.length > 0){
+        //    let arr = company.dataList.list;
+        //    let newArr = [];
+        //    for(let i=0;i<arr.length;i++){
+        //        if(arr[i].name == cName){
+        //            //arr[i].id = Math.random()*100;
+        //            //arr[i].name = '旸谷科技';
+        //            //arr[i].ceo = 'xiaojun';
+        //            //arr[i].link = 'www.cdsunrise.net';
+        //            //arr[i].address = '成都';
+        //            newArr.push(arr[i]);
+        //        }
+        //    }
+        //    //company.queryCompany(newArr);
+        //    //company.dataList.list = newArr;
+        //}
+
+        //分界线
+        for(let i=0;i<company.dataList.list.length;i++){
+            company.dataList.list.splice(i,company.dataList.list.length);
+        }
+        const newArr = {
+            "id": 2,
+            "name": "旸谷科技",
+            "ceo": "xiaojun",
+            "link": "http://www.cdsunrise.net/",
+            "address": "成都"
+        };
+        company.dataList.list.push(newArr);
     }
     render(){
         return (
-            <button type="button" onClick={this.changePackage.bind(this)}>改变下面的version和description</button>
+            <div>
+                <input type="text" ref="companyName" placeholder="输入企业名称查询" />
+                <button type="button" onClick={this.findCompany.bind(this)}>查询</button>&nbsp;
+            </div>
         );
     }
 }
-
 // 被改变状态的组件是观察者，observer
 @observer
 class UserTable extends Component{
     constructor(props){
         super(props);
         this.state = {
-            list:user.userTable.list
+            list:''
         };
     }
+    componentDidMount(){
+        $.ajax({
+            url:'./lib/user.json',
+            success:(res) => {
+                //异步获取的数据如果要做状态管理,一定要把获取的数据先传给状态管理容器,再把自身状态绑定到容器里
+                user.dataList.list = res.data;
+                this.setState({
+                    list:user.dataList.list
+                });
+
+            }
+        });
+    }
     render(){
-        return (
-            <table border="0" cellSpacing="0" cellPadding="0" width="100%">
-                <thead><tr><td>姓名</td><td>年龄</td><td>手机</td><td>地址</td></tr></thead>
-                <tbody>
+        if(this.state.list.length > 0){
+            return (
+                <table border="0" cellSpacing="0" cellPadding="0" width="100%">
+                    <thead><tr><td>姓名</td><td>年龄</td><td>手机</td><td>地址</td></tr></thead>
+                    <tbody>
                     {
                         this.state.list.map((item,index) => {
                             return (
@@ -73,45 +119,65 @@ class UserTable extends Component{
                             );
                         })
                     }
-                </tbody>
-            </table>
-        );
+                    </tbody>
+                </table>
+            );
+        }else{
+            return 'loading...';
+        }
     }
 }
 
 @observer
-class PackageJsonInfo extends Component{
+class CompanyTable extends Component{
     constructor(props){
         super(props);
         this.state = {
-            demoJson:packageJson.demoJson
-        };
+            list:''
+        }
     }
     componentDidMount(){
         $.ajax({
-            url:'./lib/demo.json',
+            url:'./lib/company.json',
             method:'GET',
             dataType:'JSON',
             success:(data) => {
-                //异步获取的数据如果要做状态管理
-                //一定要把获取的数据先传给状态管理容器
-                //再把自身状态绑定到容器里
-                packageJson.demoJson = data;
+                //异步获取的数据如果要做状态管理,一定要把获取的数据先传给状态管理容器,再把自身状态绑定到容器里
+                company.dataList.list = data.list;
                 this.setState({
-                    demoJson:packageJson.demoJson
+                    list:company.dataList.list
                 });
             }
         });
     }
     render(){
-        return (
-            <div>
-                <pre>version:{this.state.demoJson.version}</pre>
-                <pre>description:{this.state.demoJson.description}</pre>
-            </div>
-        );
+        if(this.state.list.length > 0){
+            return (
+                <table border="0" cellSpacing="0" cellPadding="0" width="100%">
+                    <thead><tr><td>序号</td><td>公司名称</td><td>CEO</td><td>公司官网</td><td>所在地</td></tr></thead>
+                    <tbody>
+                    {
+                        this.state.list.map((item,index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.ceo}</td>
+                                    <td>{item.link}</td>
+                                    <td>{item.address}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                    </tbody>
+                </table>
+            );
+        }else{
+            return 'loading...';
+        }
     }
 }
+
 
 class App extends Component{
     constructor(props){
@@ -119,11 +185,11 @@ class App extends Component{
     }
     render(){
         return [
-            <AddUser key="addUser" />,
-            <UserTable key="userTable" />,
-            <p key="华丽的分界线" style={{background:'green',textAlign:'center',color:'#fff'}}>华丽的分界线</p>,
-            <ChangePackage key="package" />,
-            <PackageJsonInfo key="testDemoJson" />
+            <PlayUser key="addUser" />,
+            <UserTable key="userTable" users={user} />,
+            <p key="华丽的分界线1" style={{background:'green',textAlign:'center',color:'#fff'}}>华丽的分界线</p>,
+            <QueryCompany key="queryCompany" />,
+            <CompanyTable key="CompanyTable" companys={company} />
         ];
     }
 }
--- web/js/store.js
+++ web/js/store.js
@@ -2,40 +2,34 @@
  * 2018/5/23
  * administractor
  */
-import {observable,action} from 'mobx';
+import {observable,computed,action} from 'mobx';
 //observable，状态是被观察者
 //action，暴露给组件监听的action
-
-//示例一(首先在容器中初始化state)
+//示例一
 export class User {
     @observable
-    userTable = {
-        list:[
-            {"id": 1,"name": "Pety","age": "10岁","mobile":13552653265,"address":"西航港双华路168号5栋"},
-            {"id": 2,"name": "Andy","age": "12岁","mobile":15826598652,"address":"西航港双华路168号11栋"},
-            {"id": 3,"name": "HanMeimei","age": "11岁","mobile":13584652697,"address":"西航港双华路168号3栋"},
-            {"id": 4,"name": "LiLei","age": "15岁","mobile":18825649857,"address":"西航港双华路168号9栋"}
-        ]
-    };
+    dataList = {list:''};
     @action
     addUser = (data) => {
-        this.userTable.list.unshift(data);//向数组开头添加一组数据
+        this.dataList.list.unshift(data);//向数组开头添加一组数据
     };
     @action
     delUser = (number) => {
-        this.userTable.list.splice(number,1);//删除下标为number的一条数据
+        this.dataList.list.splice(number,1);//删除下标为number的一条数据
     }
 }
-//示例二(异步获取的数据作状态管理)
-export class PackageJson{
+//示例二
+export class Company{
     @observable
-    demoJson = '';
+    dataList = {list:''};
     @action
-    changeVersion = (val) => {
-        this.demoJson.version = val;
-    }
-    @action
-    changeDescription = (val) => {
-        this.demoJson.description = val;
+    queryCompany = (arr) => {
+        //console.log(JSON.stringify(id));
+        //for(let i=0;i<this.dataList.list.length;i++){
+        //    if(this.dataList.list[i].id == id){
+        //        this.dataList.list.splice(i,1);
+        //    }
+        //}
+        console.log(JSON.stringify(arr));
     }
 }

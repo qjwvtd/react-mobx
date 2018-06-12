37c0e8d6d63fa3fb0cfec0d530c5efb479b83ebd
qjwvtd <176156562@qq.com>
2018-06-12 10:01:25 +0800

    no

--- web/js/button.js
+++ web/js/button.js
@@ -22,13 +22,18 @@ export default class Button extends Component{
         companyDom.innerHTML = '';
         companyDom.innerHTML = JSON.stringify(company);
     }
+    clearAll(){
+        user.dataList.list = [];
+        company.dataList.list = [];
+    }
     render(){
         return (
             [
                 <button key="showUserBtn" type="button" onClick={this.showUserInfo.bind(this)}>click me,showUserInfo</button>,
                 <div key="userinfo" style={{width:'360px'}} ref="showUser">{JSON.stringify(user)}</div>,
-                <button key="showUserBtn" type="button" onClick={this.showCompanyInfo.bind(this)}>click me,showCompanyInfo</button>,
-                <div key="userinfo" style={{width:'360px'}} ref="showCompany">{JSON.stringify(company)}</div>
+                <button key="showCompanyBtn" type="button" onClick={this.showCompanyInfo.bind(this)}>click me,showCompanyInfo</button>,
+                <div key="companyfo" style={{width:'360px'}} ref="showCompany">{JSON.stringify(company)}</div>,
+                <button key="clear" type="button" onClick={this.clearAll.bind(this)}>click me,clearAll</button>
             ]
         );
     }
--- web/js/index.js
+++ web/js/index.js
@@ -12,6 +12,9 @@ import Button from './button';
 ReactDOM.render(
     [
         <App key="main" />,
+        <hr key="hr1"/>,
+        <p key="p">以上是组件在同一个JS文件的引用，下面是在不同JS文件下的操作</p>,
+        <hr key="hr2"/>,
         <Button key="btn" />
     ],
 
